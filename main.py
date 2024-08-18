4e75be65a93b92ed4fb12833c79a25d4242b37a3
taoista <taoista-@hotmail.com>
2024-08-18 18:37:25 -0400

    avanzando en el proyecto

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-
-
+import yfinance as yf
+import matplotlib.pyplit as plt
 
 def main():
-    print("inmiciando el pryecto")
 
+    datos = yf.Ticker("MSFT").history(period="1y")
 
+    precio = datos['Close']
+    volumen = datos['Volume']
+
+    print(datos)
+    print(precio)
+
+# def obtener_precio():
+#     return False
 
 if __name__ == "__main__":
     main()
